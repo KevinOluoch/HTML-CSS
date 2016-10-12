2b8fb76ca99bce4cee272ad6a7d6fda2169e4a7a
Kevin Oluoch <kevin.o.oluoch@gmail.com>
2016-10-12 15:38:48 +0300

    Front End Lab

---
+++ HTMLjsCSS/externalJS.js
@@ -0,0 +1,11 @@
+
+
+<!-- 
+Seen = 0
+function myFunction() {
+    Seen += 1
+    if (Seen % 2){
+    document.getElementById("demo").style.display = "block";}
+	else {document.getElementById("demo").style.display = "none";}
+}
+ -->
